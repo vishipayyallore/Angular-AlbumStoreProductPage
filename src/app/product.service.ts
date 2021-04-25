be8cf8e532b0e9d4424a66d129bfe2b48ed707ee
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-04-25 11:10:42 +0530

    '5'

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -6,7 +6,7 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class ProductService {
 
-  _albumUrl = '../assets/album.json';
+  private _albumUrl = '../assets/album.json';
 
   constructor(private _http: Http) {
   }
@@ -14,6 +14,6 @@ export class ProductService {
   getAlbum(id: number) {
     return this._http.get(this._albumUrl)
       .map((response) => response.json());
-  }
+  } 
 
 }
