b727080a189c0fa2eb6f23c1e4e7057c63157c88
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-04-25 11:24:58 +0530

    '8'

--- src/app/album.ts
+++ src/app/album.ts
@@ -1,11 +1,9 @@
-import { Track } from "./track";
+import { Track } from './track';
+
 
 export interface Album {
     name: string;
-
     releaseDate: string;
-
     coverImage: string;
-
     tracks: Track[];
 }
